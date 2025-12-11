e3f92e5895cfe52ad545a600739b8607d56505cf
ramirezjesus12cr01-lang <ramirez.jesus.12cr01@gmail.com>
2025-12-11 14:33:48 -0600

    Delete start.sh

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Reemplazamos python3 por 'python' para mayor compatibilidad de entorno en Render
-# Además, cambiamos el comando de inicio para que Flask lo maneje directamente.
-exec gunicorn --bind 0.0.0.0:$PORT servidor_final:app
